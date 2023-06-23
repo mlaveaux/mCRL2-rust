3f982b93c60badf2bea59fb6874a8e85cfe2af09
Maurice Laveaux <m.laveaux@tue.nl>
2023-06-23 13:56:27 +0200

    Tried to make the address sanitizer work for MacOS as well

--- libraries/xtask/src/main.rs
+++ libraries/xtask/src/main.rs
@@ -132,24 +132,39 @@ fn coverage() -> AnyResult<()> {
     println!("=== cleaning up ===");
     clean_files("**/*.profraw")?;
     println!("ok.");
-    
+
     println!("report location: {file}");
 
     Ok(())
 }
 
+#[cfg(not(unix))]
+fn add_target_flag(arguments: &mut Vec<String>) {}
+
+#[cfg(target_os = "linux")]
+fn add_target_flag(arguments: &mut Vec<String>) {
+    arguments.push("--target".to_string());
+    arguments.push( "x86_64-unknown-linux-gnu".to_string());
+}
+
+#[cfg(target_os = "macos")]
+fn add_target_flag(arguments: &mut Vec<String>) {
+    arguments.push("--target".to_string());
+    arguments.push( "x86_64-apple-darwin".to_string());
+}
+
 ///
 /// Run the tests with the address sanitizer enabled to detect memory issues in unsafe and C++ code.
 ///
-/// This only works under Linux currently and requires the nightly toolchain
+/// This only works under Linux and MacOS currently and requires the nightly toolchain.
 ///
 fn sanitizer(cargo_arguments: Vec<String>) -> AnyResult<()> {
     let mut arguments: Vec<String> = vec![
         "test".to_string(),
         "-Zbuild-std".to_string(),
-        "--target".to_string(),
-        "x86_64-unknown-linux-gnu".to_string(),
     ];
+
+    add_target_flag(&mut arguments);
 
     arguments.extend(cargo_arguments.into_iter());
 
