64d5ac3a62585bfa7f83e55eb65ec05916d12d8a
Maurice Laveaux <m.laveaux@tue.nl>
2023-12-31 20:00:30 +0100

    Print a table from the benchmarks.
    # Conflicts:
    #	Cargo.lock
    #	Cargo.toml
    #	libraries/xtask/Cargo.toml
    #	libraries/xtask/src/benchmark.rs

--- libraries/xtask/src/benchmark.rs
+++ libraries/xtask/src/benchmark.rs
@@ -1,6 +1,7 @@
-use std::{error::Error, fs, env};
+use std::{error::Error, fs, env, io::BufRead};
 
 use duct::cmd;
+use regex::Regex;
 
 pub fn benchmark() -> Result<(), Box<dyn Error>> {
     // Build the tool with the correct settings
@@ -9,6 +10,11 @@ pub fn benchmark() -> Result<(), Box<dyn Error>> {
     // Using which is a bit unnecessary, but it deals nicely with .exe on Windows and can also be used to do other searching.
     let cwd = env::current_dir()?;
     let mcrl2_rewrite = which::which_in("mcrl2rewrite", Some("target/release/"), cwd)?;
+
+    let mcrl2_rewrite_timing = Regex::new(r#"rewriting: ([0-9]*) milliseconds."#).unwrap();
+
+    // Keep track of the resulting timing for every benchmark.
+    let mut results = vec![];
 
     // Take every benchmark
     for file in fs::read_dir("examples/REC/mcrl2")? {
@@ -20,18 +26,49 @@ pub fn benchmark() -> Result<(), Box<dyn Error>> {
             let expressions = path.with_extension("expressions");
 
             let benchmark = path.file_stem().unwrap();
+            let benchmark = String::from(benchmark.to_string_lossy());
 
-            println!("Benchmarking {}", benchmark.to_string_lossy());
+            println!("Benchmarking {}", benchmark);
             
             // Strip the beginning UNC path even through technically correct hyperfine does not deal with it properly.
 
-            cmd!(&mcrl2_rewrite,
+            match cmd!(&mcrl2_rewrite,
                 "--rewriter",
                 "innermost",
                 format!("{}", data_spec.to_string_lossy()), 
                 format!("{}", expressions.to_string_lossy())
-            ).run()?;
+            )
+            .stderr_capture()
+            .run() {
+                Ok(result) => {
+                    // Parse the standard output to read the rewriting time and insert it into results.
+                    for line in result.stderr.lines() {     
+                        if let Some(result ) = mcrl2_rewrite_timing.captures(&line.unwrap()) {
+                            let (_, [grp1]) = result.extract();                            
+                            let timing: usize = grp1.parse()?;
+
+                            println!("Benchmark {} timing {} milliseconds", benchmark, timing);
+                            results.push((benchmark.clone(), timing))
+                        }
+                    }
+                },
+                Err(err) => {
+                    println!("Benchmark {} timed out or crashed", benchmark);
+                    results.push((benchmark.clone(), usize::MAX));
+
+                    println!("Command failed {:?}", err);
+                }
+            };
         }
+    }
+
+    // Print the results in alphabetical order.
+    results.sort_unstable_by(|(name, _), (name2, _)| {
+        human_sort::compare(name, name2)
+    });
+
+    for (benchmark, timing) in results {        
+        println!("{: >30} | {: >10}", benchmark, timing);
     }
 
     Ok(())
