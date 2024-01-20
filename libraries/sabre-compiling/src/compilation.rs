ab1671d969129ec4ace5038b8b0afe620407b0b1
Maurice Laveaux <m.laveaux@tue.nl>
2024-01-20 14:49:56 +0100

    Added a subcrate to put the compiled code in.

--- libraries/sabre-compiling/src/compilation.rs
+++ libraries/sabre-compiling/src/compilation.rs
@@ -1,23 +1,16 @@
 use std::{fs::File, error::Error, io::Write};
 
-
-
+use indoc::indoc;
 
 pub fn compile() -> Result<(), Box<dyn Error>> {
 
     // Write the output source file.
-
-    let mut file = File::create("output.rs")?;
-
-    writeln!(&mut file, "
+    let mut file = File::create("sabre-compiled/src/lib.rs")?;
+    writeln!(&mut file, indoc! {"
         fn main() {{
             println!(\"Hello world!\");
         }}
-    ")?;
-
-
-
-    
+    "})?;
 
     Ok(())
 }
