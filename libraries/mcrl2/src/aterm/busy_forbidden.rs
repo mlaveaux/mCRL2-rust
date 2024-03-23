799410bfaa33148b3f43277f246f440319ee1252
Maurice Laveaux <m.laveaux@tue.nl>
2024-03-24 00:39:55 +0100

    This counter cannot be inside the busy forbidden protocol, but must be thread local.

--- libraries/mcrl2/src/aterm/busy_forbidden.rs
+++ libraries/mcrl2/src/aterm/busy_forbidden.rs
@@ -1,22 +1,16 @@
 use std::{
-    cell::{Cell, UnsafeCell},
+    cell::UnsafeCell,
     marker::PhantomData,
     ops::{Deref, DerefMut},
 };
 
 use mcrl2_sys::atermpp::ffi;
 
-const TEST_GC_INTERVAL: usize = 100;
-
 /// Provides access to the mCRL2 busy forbidden protocol, where there
 /// are thread local busy flags and one central storage for the forbidden
 /// flags. Care must be taken to avoid deadlocks since the FFI also uses
 /// the same flags.
 pub struct BfTermPool<T: ?Sized> {
-    /// This is a stupid hack, but we need to periodically test for garbage collection and this is only allowed outside of a shared
-    /// lock section. Therefore, we count (arbitrarily) to reduce the amount of this is checked.
-    gc_counter: Cell<usize>,
-
     object: UnsafeCell<T>,
 }
 
@@ -27,7 +21,6 @@ impl<T> BfTermPool<T> {
     pub fn new(object: T) -> BfTermPool<T> {
         BfTermPool {
             object: UnsafeCell::new(object),
-            gc_counter: Cell::new(TEST_GC_INTERVAL),
         }
     }
 }
@@ -69,20 +62,6 @@ impl<'a, T: ?Sized> BfTermPool<T> {
             _marker: Default::default(),
         }
     }
-
-    /// Decrements the garbage collection counter and returns true iff the counter reached zero.
-    fn decrement_gc_counter(&self) -> bool {        
-        let mut gc_counter = self.gc_counter.take();
-
-        gc_counter -= 1;
-        if gc_counter == 0 {
-            gc_counter = TEST_GC_INTERVAL;
-        }
-
-        self.gc_counter.set(gc_counter);
-
-        gc_counter == TEST_GC_INTERVAL
-    }
 }
 
 pub struct BfTermPoolRead<'a, T: ?Sized> {
@@ -102,9 +81,7 @@ impl<'a, T: ?Sized> Deref for BfTermPoolRead<'a, T> {
 impl<'a, T: ?Sized> Drop for BfTermPoolRead<'a, T> {
     fn drop(&mut self) {
         // If we leave the shared section and the counter is zero.
-        if ffi::unlock_shared() && self.mutex.decrement_gc_counter() {
-            ffi::test_garbage_collection();
-        }
+        ffi::unlock_shared();
     }
 }
 
@@ -132,10 +109,6 @@ impl<'a, T: ?Sized> DerefMut for BfTermPoolWrite<'a, T> {
 impl<'a, T: ?Sized> Drop for BfTermPoolWrite<'a, T> {
     fn drop(&mut self) {
         ffi::unlock_exclusive();
-
-        if self.mutex.decrement_gc_counter() {
-            ffi::test_garbage_collection();
-        }
     }
 }
 
@@ -164,10 +137,7 @@ impl<'a, T: ?Sized> DerefMut for BfTermPoolThreadWrite<'a, T> {
 impl<'a, T: ?Sized> Drop for BfTermPoolThreadWrite<'a, T> {
     fn drop(&mut self) {
         if self.locked {
-            // If we leave the shared section and the counter is zero.
-            if ffi::unlock_shared() && self.mutex.decrement_gc_counter() {
-                ffi::test_garbage_collection();
-            }
+            ffi::unlock_shared();
         }
     }
 }
