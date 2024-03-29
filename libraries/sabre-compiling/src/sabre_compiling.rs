6142500265a71d952e4cdc303dc61cb3b01c7a64
Maurice Laveaux <m.laveaux@tue.nl>
2024-03-29 17:47:34 +0100

    Split the implementation into several modules for readability.

---
+++ libraries/sabre-compiling/src/sabre_compiling.rs
@@ -0,0 +1,112 @@
+use std::{
+    cell::RefCell, error::Error, fs::File, io::Write, path::{Path, PathBuf}, rc::Rc
+};
+
+use indoc::indoc;
+use libloading::{Library, Symbol};
+use log::info;
+use temp_dir::TempDir;
+use toml::Table;
+
+use mcrl2::{aterm::TermPool, data::DataExpression};
+use sabre::{set_automaton::SetAutomaton, RewriteEngine, RewriteSpecification};
+
+use crate::library::RuntimeLibrary;
+
+pub struct SabreCompilingRewriter {
+    library: Library,
+    //rewrite_func: Symbol<unsafe extern fn() -> u32>,
+}
+
+impl RewriteEngine for SabreCompilingRewriter {
+    fn rewrite(&mut self, term: DataExpression) -> DataExpression {
+        term
+    }
+}
+
+impl SabreCompilingRewriter {
+    /// Creates a new compiling rewriter for the given specifications.
+    ///
+    /// - use_local_workspace: Use the developement version of the toolset instead of referring to the github one.
+    /// - use_local_tmp: Use a relative 'tmp' directory instead of using the system directory. Mostly used for debugging purposes.
+    pub fn new(
+        tp: Rc<RefCell<TermPool>>,
+        spec: &RewriteSpecification,
+        use_local_workspace: bool,
+        use_local_tmp: bool,
+    ) -> Result<SabreCompilingRewriter, Box<dyn Error>> {
+        let apma = SetAutomaton::new(spec, |_| (), true);
+
+        let system_tmp_dir = TempDir::new()?;
+        let temp_dir = if use_local_tmp {
+            &Path::new("./tmp")
+        } else {
+            system_tmp_dir.path()
+        };
+
+        let mut dependencies = vec![];
+
+        if use_local_workspace {
+            let compilation_toml = include_str!("../Compilation.toml").parse::<Table>()?;
+            let path = compilation_toml
+            .get("sabrec")
+            .ok_or("Missing [sabre] section)")?
+            .get("path")
+            .ok_or("Missing path entry")?
+            .as_str()
+            .ok_or("Not a string")?;
+
+            info!("Using local dependency {}", path);
+            dependencies.push(format!(
+                "sabre-ffi = {{ path = \"{}\" }}", PathBuf::from(path).join("sabre-ffi").to_string_lossy()             
+            ));
+        } else {
+            info!("Using git dependency https://github.com/mlaveaux/mCRL2-rust.git");
+            dependencies.push(
+                "sabre-ffi = { git = \"https://github.com/mlaveaux/mCRL2-rust.git\" }".to_string(),
+            );
+        }
+
+        let mut compilation_crate = RuntimeLibrary::new(temp_dir, dependencies)?;
+
+        // Write the output source file(s).
+        {
+            let mut file =
+                File::create(PathBuf::from(compilation_crate.source_dir()).join("lib.rs"))?;
+            writeln!(
+                &mut file,
+                indoc! {"
+                #[no_mangle]
+                pub unsafe extern \"C\" fn rewrite_term() {{
+                    println!(\"Hello world!\");
+                }}
+            "}
+            )?;
+        }
+
+        let library = compilation_crate.compile()?;
+
+        unsafe {
+            let func: Symbol<extern "C" fn()> = library.get(b"rewrite_term")?;
+
+            func();
+        }
+
+        Ok(SabreCompilingRewriter { library })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use test_log::test;
+
+    #[test]
+    fn test_compilation() {
+        let spec = RewriteSpecification::default();
+        let tp = Rc::new(RefCell::new(TermPool::new()));
+
+        SabreCompilingRewriter::new(tp, &spec, true, false).unwrap();
+    }
+}
