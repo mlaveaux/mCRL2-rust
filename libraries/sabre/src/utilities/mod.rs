2574a16ab2cf457f61c8a2328bc3b8a525630d88
Maurice Laveaux <m.laveaux@tue.nl>
2024-04-16 10:46:29 +0200

    Revert "Added a MatchTerm to avoid unnecessary construction."
    This reverts commit 1ee3827f42d4044584504484f21cd3735838769f.

--- libraries/sabre/src/utilities/mod.rs
+++ libraries/sabre/src/utilities/mod.rs
@@ -1,13 +1,11 @@
 mod configuration_stack;
 mod innermost_stack;
-mod match_term;
 mod position;
 mod semi_compressed_tree;
 mod substitution;
 
-pub use match_term::*;
+pub(crate) use innermost_stack::*;
+pub(crate) use configuration_stack::*;
 pub use position::*;
 pub use semi_compressed_tree::*;
 pub use substitution::*;
-pub(crate) use configuration_stack::*;
-pub(crate) use innermost_stack::*;
