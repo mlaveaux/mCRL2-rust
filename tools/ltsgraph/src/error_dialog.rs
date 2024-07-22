dbbcd14c0d512fb9f9a343eba8f8644196f340e6
Maurice Laveaux <m.laveaux@tue.nl>
2024-07-22 15:24:28 +0200

    Moved the error dialog to the slint file.

--- tools/ltsgraph/src/error_dialog.rs
+++ tools/ltsgraph/src/error_dialog.rs
@@ -1,26 +1,4 @@
-use slint::slint;
-
-slint! {
-    import { StandardButton, Button } from "std-widgets.slint";
-
-    export component ErrorDialog inherits Dialog {
-        min-width: 100px;
-        min-height: 100px;
-        title: error_title;
-
-        in property <string> error_text: "Message";
-        in property <string> error_title: "Error!";
-
-        VerticalLayout {
-            Text {
-                vertical-alignment: center;
-                text: error_text;
-            }
-        }
-
-        StandardButton { kind: ok; }
-    }
-}
+slint::include_modules!();
 
 /// Shows a standard error dialog with the given title and message, blocking
 /// input until closed or "ok" is pressed.
