c10141e864f4c2cff5821e0395c2c2185ed2fdaa
Maurice Laveaux <m.laveaux@tue.nl>
2024-08-11 13:40:30 +0200

    Used the functionality in the unsafety crate.

--- libraries/sabre/src/lib.rs
+++ libraries/sabre/src/lib.rs
@@ -2,6 +2,9 @@
 //! The Set Automaton Based Rewrite Engine (Sabre) implements a rewriter for
 //! conditional first-order non-linear rewrite rules.
 //!
+//! This crate does not use unsafe code.
+
+//#![forbid(unsafe_code)]
 
 pub mod innermost_rewriter;
 pub mod matching;
