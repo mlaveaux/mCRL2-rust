42d475d3b339d9af6f4ed43d70e5f1594384d366
Maurice Laveaux <m.laveaux@tue.nl>
2024-08-14 17:38:56 +0200

    Fixed some issues with generating random LTSs

--- libraries/lts/src/random_lts.rs
+++ libraries/lts/src/random_lts.rs
@@ -21,18 +21,18 @@ pub fn random_lts(num_of_states: usize, num_of_labels: u32, outdegree: usize) ->
 
     for state in &mut states {
 
-        // Pick a random label and state.
-        let label = rng.gen_range(0..num_of_labels);
-        let to = rng.gen_range(0..num_of_states);
-
         // Introduce outgoing transitions for this state based on the desired out degree.
-        for _ in 0..outdegree {
+        for _ in 0..rng.gen_range(0..outdegree) {
+            // Pick a random label and state.
+            let label = rng.gen_range(0..num_of_labels);
+            let to = rng.gen_range(0..num_of_states);
+
             state.outgoing.push((label as usize, to));
             num_of_transitions += 1;
         }
     }
 
-    LabelledTransitionSystem::new(0, states, labels, num_of_transitions)
+    LabelledTransitionSystem::new(0, states, labels, Vec::new(), num_of_transitions)
 }
 
 #[cfg(test)]
