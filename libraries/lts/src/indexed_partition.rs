3a82fd59d6d9f2deb0a0e28f2bc19ecaef469fdb
Maurice Laveaux <m.laveaux@tue.nl>
2024-08-15 16:16:45 +0200

    Fixed printing, made partition keep track of the number of blocks.

--- libraries/lts/src/indexed_partition.rs
+++ libraries/lts/src/indexed_partition.rs
@@ -7,6 +7,8 @@ use crate::Partition;
 #[derive(Debug)]
 pub struct IndexedPartition {
     partition: Vec<usize>,
+
+    num_of_blocks: usize,
 }
 
 impl IndexedPartition {
@@ -14,17 +16,16 @@ impl IndexedPartition {
     /// Create a new partition where all elements are in a single block.
     pub fn new(num_of_elements: usize) -> IndexedPartition {
         IndexedPartition {
-            partition: vec![0; num_of_elements]
+            partition: vec![0; num_of_elements],
+            num_of_blocks: 1,
         }
-    }
-
-    /// Returns a mutable iterator over all elements in the partition.
-    pub fn iter_mut<'a>(&'a mut self) -> impl Iterator<Item = &mut usize> + 'a {
-        self.partition.iter_mut()
     }
 
     /// Sets the block number of the given element
     pub fn set_block(&mut self, element_index: usize, block_number: usize) {
+        // TODO: This assumes that the blocks are dense, otherwise it overestimates the number of blocks.
+        self.num_of_blocks = self.num_of_blocks.max(block_number + 1);
+
         self.partition[element_index] = block_number;
     }
 }
@@ -34,7 +35,6 @@ impl fmt::Display for IndexedPartition {
         write!(f, "{{ ")?;
 
         let mut first = true;
-        let mut last_empty = true;
 
         for block_index in 0..self.partition.len() {
 
@@ -46,7 +46,7 @@ impl fmt::Display for IndexedPartition {
                 if !first_block {
                     write!(f, ", ")?;
                 } else {
-                    if !first && last_empty {
+                    if !first {
                         write!(f, ", ")?;
                     }
 
@@ -59,10 +59,8 @@ impl fmt::Display for IndexedPartition {
 
             if !first_block {
                 write!(f, "}}")?;
+                first = false;
             }
-
-            first = false;
-            last_empty = !first_block;
         }
 
         write!(f, " }}")
@@ -75,15 +73,6 @@ impl Partition for IndexedPartition {
     }
 
     fn num_of_blocks(&self) -> usize {
-        // Figure out the highest block number for the number of states.
-        // TODO: This assumes that the blocks are dense, otherwise it overestimates the number of blocks.
-        match self.partition.iter().max() {
-            None => {
-                1
-            },
-            Some(max_block_number) => {
-                max_block_number + 1
-            }
-        }
+        self.num_of_blocks
     }
 }
