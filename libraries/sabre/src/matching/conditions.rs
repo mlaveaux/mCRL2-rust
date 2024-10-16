e40e741bcf976d6557da31506410547329fe9519
Maurice Laveaux <m.laveaux@tue.nl>
2024-10-16 21:24:10 +0200

    Replaced conditions by the term stack

--- libraries/sabre/src/matching/conditions.rs
+++ libraries/sabre/src/matching/conditions.rs
@@ -1,5 +1,5 @@
 use crate::utilities::create_var_map;
-use crate::utilities::SemiCompressedTermTree;
+use crate::utilities::TermStack;
 use crate::Rule;
 
 /// This is a [Rule] condition stored as semi compressed trees such that they can be
@@ -7,8 +7,8 @@ use crate::Rule;
 #[derive(Hash, Clone, Eq, PartialEq, Ord, PartialOrd, Debug)]
 pub struct EMACondition {
     /// Conditions lhs and rhs are stored in the term pool as much as possible with a SemiCompressedTermTree
-    pub semi_compressed_lhs: SemiCompressedTermTree,
-    pub semi_compressed_rhs: SemiCompressedTermTree,
+    pub semi_compressed_lhs: TermStack,
+    pub semi_compressed_rhs: TermStack,
 
     /// whether the lhs and rhs should be equal or different
     pub equality: bool,
@@ -16,13 +16,13 @@ pub struct EMACondition {
 
 /// Computes the extended condition from a given rewrite rule.
 pub fn extend_conditions(rule: &Rule) -> Vec<EMACondition> {
-    let var_map = create_var_map(&rule.lhs.clone().into());
+    let var_map = create_var_map(&rule.lhs.copy().into());
     let mut conditions = vec![];
 
     for c in &rule.conditions {
         let ema_condition = EMACondition {
-            semi_compressed_lhs: SemiCompressedTermTree::from_term(&c.lhs.copy().into(), &var_map),
-            semi_compressed_rhs: SemiCompressedTermTree::from_term(&c.rhs.copy().into(), &var_map),
+            semi_compressed_lhs: TermStack::from_term(&c.lhs.copy().into(), &var_map),
+            semi_compressed_rhs: TermStack::from_term(&c.rhs.copy().into(), &var_map),
             equality: c.equality,
         };
         conditions.push(ema_condition);
