1bd5720f77fb60662d01cb0b84193665bd887413
Maurice Laveaux <m.laveaux@tue.nl>
2024-10-20 13:59:22 +0200

    Use a hashset for quotienting the transitions to avoid quadratic insertion on high fanout

--- libraries/lts/src/reduction/quotient.rs
+++ libraries/lts/src/reduction/quotient.rs
@@ -1,3 +1,5 @@
+use rustc_hash::FxHashSet;
+
 use crate::LabelledTransitionSystem;
 use crate::State;
 
@@ -68,38 +70,38 @@ pub fn quotient_lts(
     eliminate_tau_loops: bool,
 ) -> LabelledTransitionSystem {
     // Introduce the transitions based on the block numbers
-    let mut num_of_transitions = 0;
     let mut states: Vec<State> = vec![State::default(); partition.num_of_blocks()];
+    let mut transitions: FxHashSet<(usize, usize, usize)> = FxHashSet::default();
+
     for (state_index, state) in lts.iter_states() {
-        for (label, to) in &state.outgoing {
+        for &(label, to) in &state.outgoing {
+            let block = partition.block_number(state_index);
+            let to_block = partition.block_number(to);
+
             // If we eliminate tau loops then check if the to and from end up in the same block
             if !eliminate_tau_loops
-                || !(lts.is_hidden_label(*label) && partition.block_number(state_index) == partition.block_number(*to))
+                || !(lts.is_hidden_label(label) && block == to_block)
             {
                 debug_assert!(
                     partition.block_number(state_index) < partition.num_of_blocks(),
                     "Quotienting assumes that the block numbers do not exceed the number of blocks"
                 );
 
-                let outgoing = &mut states[partition.block_number(state_index)].outgoing;
-                let to_block = partition.block_number(*to);
-
                 // Make sure to keep the outgoing transitions sorted.
-                match outgoing.binary_search(&(*label, to_block)) {
-                    Ok(_) => {} // element already in vector
-                    Err(pos) => {
-                        outgoing.insert(pos, (*label, to_block));
-                        num_of_transitions += 1;
-                    }
-                }
+                transitions.insert((block, label, to_block));
             }
         }
     }
+
+    for &(from, label, to) in &transitions {
+        states[from].outgoing.push((label, to));
+    }
+
     LabelledTransitionSystem::new(
         partition.block_number(lts.initial_state_index()),
         states,
         lts.labels().into(),
         lts.hidden_labels().into(),
-        num_of_transitions,
+        transitions.len(),
     )
 }
